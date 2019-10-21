c31afa1297d7b0fdf93fc6578a120986bc6aa53e
Vincent Troy Allen Jr <vtad4f@mst.edu>
2019-10-21 18:58:01 -0500

    Handling more commands

--- main.cpp
+++ main.cpp
@@ -25,6 +25,19 @@ namespace
       return ss.str();
    }
    
+   /////////////////////////////////////////////////////////////////////////////
+   ///
+   ///   @brief  Wait for the user to quit
+   ///
+   /////////////////////////////////////////////////////////////////////////////
+   void Block()
+   {
+      std::cout << "Press any key to quit." << std::endl;
+      
+      char whatever;
+      std::cin >> whatever;
+   }
+   
 } // end namespace
 
 
@@ -37,17 +50,31 @@ int main(int argc, char **argv)
       args.insert(argv[i]);
    }
    
-   // No args?
-   if (args.empty())
+   bool valid  = !args.empty();
+   bool help   = args.erase("-h") != args.cend() || args.erase("--help")   != args.cend();
+   bool server = args.erase("-s") != args.cend() || args.erase("--server") != args.cend();
+   bool client = args.erase("-c") != args.cend() || args.erase("--client") != args.cend();
+   valid = valid && (server || client);
+   
+   // Optional number of clients
+   int nClients = 1;
+   if (client && args.size() == 1)
    {
-      std::cerr << Help(argv[0]) << std::endl;
-      return 1;
+      nClients = std::stoi(*args.begin());
+      args.erase(args.begin())
    }
    
    // Help arg?
-   if (args.find("-h") != args.cend() || args.find("--help") != args.cend())
+   if (!valid)
    {
       std::cout << Help(argv[0]) << std::endl;
+      return 1;
+   }
+   
+   // No args?
+   if (help)
+   {
+      std::cerr << Help(argv[0]) << std::endl;
       return 1;
    }
    
@@ -56,14 +83,21 @@ int main(int argc, char **argv)
    std::cerr << "srand(" << seed << ")" << std::endl;
    srand(seed);
    
+   if (server && client) // Both
+   {
+      for (int i = 0 ; i < nClients ; ++i) { Client client; }
+      Server server;
+   }
+   else if (server) // Server
+   {
+      Server server;
+   }
+   else if (client) // Client(s)
+   {
+      for (int i = 0 ; i < nClients ; ++i) { Client client; }
+   }
    
-   
-   // Server?
-   Server server;
-   
-   // Client(s)?
-   Client client;
-   
+   Block();
    return 0;
 }
 
