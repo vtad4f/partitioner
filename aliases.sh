ce7608f5356c8aa31109457d7be3f289ceb69f29
Vincent Troy Allen Jr <vtad4f@mst.edu>
2019-11-03 22:28:40 -0600

    Tweaking the fsm run function so that it is timed

--- aliases.sh
+++ aliases.sh
@@ -7,7 +7,7 @@ function x { git update-index --chmod=+x $1 ; }
 function run.fsm
 {
    pushd fsm > /dev/null 2>&1
-   ./gSpan-64 gIndex/dataset > output.txt 2>&1
+   time ./gSpan-64 gIndex/dataset > output.txt 2>&1
    popd > /dev/null 2>&1
 }
 
