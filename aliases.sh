4672ddc5e43eb8235c74a873ec09eee52af7a4fc
Vincent Troy Allen Jr <vtad4f@mst.edu>
2019-11-03 22:21:36 -0600

    Adding another alias for running gspan

--- aliases.sh
+++ aliases.sh
@@ -4,3 +4,10 @@ function clean { git clean -dfqX ; }
 function reload { git fetch ; git checkout -B master origin/master ; }
 function x { git update-index --chmod=+x $1 ; }
 
+function run.fsm
+{
+   pushd fsm > /dev/null 2>&1
+   ./gSpan-64 gIndex/dataset > output.txt 2>&1
+   popd > /dev/null 2>&1
+}
+
